fdfbc02e94adf59a55f76e3bf5ebae539926a645
ussouza <marcelo.souza@unitedsystems.com.br>
2026-09-22 09:19:21 -0300

    feat: register CBS persistence and return processing services

--- src/RadarWeb.Net.ApuracaoAssistida.Api/Program.cs
+++ src/RadarWeb.Net.ApuracaoAssistida.Api/Program.cs
@@ -1,6 +1,17 @@
+using RadarWeb.Net.ApuracaoAssistida.Application.Interfaces;
+using RadarWeb.Net.ApuracaoAssistida.Infrastructure.Data;
+using RadarWeb.Net.ApuracaoAssistida.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient("ReceitaCbs");
+builder.Services.AddDbContext<ApuracaoAssistidaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApuracaoAssistida")));
+
+builder.Services.AddScoped<IApuracaoCbsRepository, ApuracaoCbsRepository>();
+builder.Services.AddScoped<IRetornoCbsService, RetornoCbsService>();
 
 var app = builder.Build();
 
